18328efe0c3d31954fe8e0b9042f7730c66298b7
katafuchix <katafuchix@gmail.com>
2026-06-10 03:10:57 +0900

    Initial Commit

---
+++ MapBite_Example/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MapBite_Example
+//
+//  Created by cano on 2026/06/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MapBite_Example/MapBite_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapBite_ExampleApp.swift
+//  MapBite_Example
+//
+//  Created by cano on 2026/06/10.
+//
+
+import SwiftUI
+
+@main
+struct MapBite_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
